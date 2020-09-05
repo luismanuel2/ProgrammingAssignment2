f64439abf37b16bcafd8344ce0dda6341074240b
luismanuel2 <luisambrocio@comunidad.unam.mx>
2020-09-05 00:08:58 -0500

    ini

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {##creates an "array" object 
+  inv<-NULL
+  x<-matrix(data = c(10,7,-10,-9,-12,10,-12,11,3),nrow = 3,ncol = 3)##It gives initial values to the array
+  set <- function(y) {## modify the array
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x##get the array
+  setinv <- function(inverza) inv <<- inverza##set the matrix inverse
+  getinv <- function() inv ##get the matrix inverse
+  list(set = set, get = get,## list with the 4 functions
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {##receives a list that is obtained by calling the above function
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {##returns the matrix inverse  if it exists
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)##computes the matrix inverse  if it does not exist
+  x$setinv(inv)
+  inv
 }
